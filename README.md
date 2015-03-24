ef3492396e46c25e90d540a68bcf7371003027d5
stereobird <stereobird@gmail.com>
2015-03-23 20:47:14 -0700

    Playable state
    Adding SASS, improvements to interaction, starting on responsiveness.

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# sudoku
+# Sudoku
+
+This is an in-progress, basic Sudoku game that relies heavily on jQuery.
+
+Current status is: playable, but not super-friendly. Generates a random, solvable board on-demand, validates without excitement.
+
+
+## Upcoming
+Need:
+- Mobile playability improvement
+
+Considering:
+- A game timer
+- In-game hints
+- Better validation (a "solve" button that will correct/show mistakes, fill in empty cells)
+- Difficulty selector
+
+
+## Rough Changelog
+
+#### 2015-03-23
+A playable board!
+- Introduced SASS for CSS preprocessing.
+- Has a "test" button to provide feedback about board status.
+- Characters limited to 1-9
+- Use a keyboard or use the number buttons on-screen to enter numbers in an active cell
+- Rudimentary responsiveness (max 500px board, shrinks to as small as 200px)
+
+I think I'll rework the entire validation approach so the game can provide more meaningful feedback, or solve the board with indicators of user error(s).
+
+#### 2015-03-22
+Basic board structure and board generation, validation.
--- js/sudoku.js
+++ js/sudoku.js
@@ -1,122 +1,180 @@
-$(function(){
-
-    var sudoku = {
-        // generate a basic number grid (81 numbers in a "solved" but generic state)
-        generate: function() {
-            var nums = [],
-                i,
-                ii;
-            for (i = 0; i < 9; i++) {
-                for (ii = 0; ii < 9; ii++) {
-                    nums.push((i*3 + Math.floor(i/3) + ii) % 9 + 1);
-                }
-            }
-            sudoku.refresh(sudoku.disorder(nums));
-        },
-        // mixup the numbers in the array, but sort of thoughtfully
-        disorder: function(nums) {
-            var i, jumbles = 75, from_number, to_number, ii;
-            for (i = 1; i < jumbles; ++i) {
-                from_number = Math.floor(Math.random() * 9) + 1; // 3
-                to_number   = Math.floor(Math.random() * 9) + 1; // 6
-                for (ii = 0; ii < nums.length; ii++) {
-                    if (nums[ii] == from_number) {
-                        nums[ii] = to_number;
-                    } else if (nums[ii] == to_number) {
-                        nums[ii] = from_number;
-                    }
-                }
-            }
-            return nums;
-        },
-        refresh: function(nums) {
-            // very crudely, we'll just randomize hiding of some numbers
-            $.each(nums, function(i,v) {
-                // ~30% of the time, don't insert the number
-                var rnd = Math.random();
-                if (rnd > 0.32) {
-                    $("#x" + (i+1)).val(v);
-                } else {
-                    $("#x" + (i+1)).val("");
-                }
-
-            });
-        },
-        validate: function(type, i) {
-            var nums = [],
-                sum  = 0,
-                num;
-            $.each($("." + type + i + " input"), function(){
-                num = parseInt($(this).val());
-                // make sure it's a number, it's not infinity, it's not a decimal, it's between 0 and 10
-                if (typeof num === "number" && isFinite(num) && num % 1 === 0 && num > 0 && num < 10) {
-                    // make sure number is unique
-                    if ($.inArray(num, nums) === -1) {
-                        nums.push(num);
-                        sum += num;
-                    } else {
-                        nums.push(0);
-                    }
-                }
-            });
-            // validate length
-            if (nums.length === 9) {
-                // should total 45 (if not, a number was duplicated somewhere in this collection)
-                if (sum === 45) {
-                    // all good
-                    return 0;
-                } else {
-                    // number duplicated in col
-                    return 1;
-                }
-            } else {
-                // not enough valid numbers in col
-                return 2;
+var sudoku = {
+    // generate a basic number grid (81 numbers in a "solved" but generic state)
+    generate: function() {
+        var nums = [],
+            i,
+            ii;
+        for (i = 0; i < 9; i++) {
+            for (ii = 0; ii < 9; ii++) {
+                nums.push((i*3 + Math.floor(i/3) + ii) % 9 + 1);
             }
         }
-    };
+        sudoku.refresh(sudoku.disorder(nums));
+        $("#status").html("");
+    },
+    // mixup the numbers in the array, but sort of thoughtfully
+    disorder: function(nums) {
+        var i,
+            freq = 75,
+            orig,
+            dest,
+            ii;
+        for (i = 1; i < freq; ++i) {
+            orig = Math.floor(Math.random() * 9) + 1;
+            dest = Math.floor(Math.random() * 9) + 1;
+            for (ii = 0; ii < nums.length; ii++) {
+                if (nums[ii] === orig) {
+                    nums[ii] = dest;
+                } else if (nums[ii] === dest) {
+                    nums[ii] = orig;
+                }
+            }
+        }
+        return nums;
+    },
+    refresh: function(nums) {
+        var rnd;
+        // very crudely, we'll just randomize hiding of some numbers
+        $.each(nums, function(i,v) {
+            rnd = Math.random();
+            // ~30% of the time, don't insert the number
+            if (rnd > 0.32) {
+                $("#x" + (i+1)).val(v);
+                $("#x" + (i+1)).attr("type", "text").attr("readonly", "readonly").parent().removeClass("empty").addClass("filled");
+            } else {
+                $("#x" + (i+1)).val("");
+                $("#x" + (i+1)).removeAttr("readonly").parent().removeClass("filled").addClass("empty");
+            }
 
-
-
-// debugging for now...
-
-    // validate board
-    $("#btnvalidate").on("click", function(){
+        });
+    },
+    validate: function() {
         var rowerrors = [],
             colerrors = [],
-            sqrerrors = [],
-            i;
-
-        // loop throuh all rows/cols/squares 9x
+            regerrors = [],
+            i,
+            dupes     = "",
+            empties   = "";
+        // loop throuh all rows/cols/regions 9x
         for (i = 1; i < 10; ++i) {
             // check row
-            rowerrors.push(sudoku.validate("row", i));
+            rowerrors.push(sudoku.analyze("row", i));
             // check column
-            colerrors.push(sudoku.validate("col", i));
-            // check square
-            sqrerrors.push(sudoku.validate("sq", i));
+            colerrors.push(sudoku.analyze("col", i));
+            // check region
+            regerrors.push(sudoku.analyze("reg", i));
         }
-
-        // all zeroes means no errors in the row/column/square [0,0,0,0,0,0,0,0,0]
-        // 1 indicates a duplicated number, e.g. [0,0,0,1,0,1,1,1,0]
-        // 2 indictaes an empty space or invalid charcater, e.g. [0,0,2,2,0,0,0,0,0]
+        // all zeroes means no errors in the row, column or region; e.g. [0,0,0,0,0,0,0,0,0]
+        // 1 indicates a duplicated number in the row, column or region; e.g. [0,0,0,1,0,1,1,1,0]
+        // 2 indictaes an empty space in the row, column or region; e.g. [0,0,2,2,0,0,0,0,0]
         // (2 takes precedence)
+        if (rowerrors.length !== 9 || colerrors.length !== 9 || regerrors.length !== 9) {
+            // something has gone wrong, one or more groups could not be validated
+            // (this should never happen)
+            $("#status").html("A horrible error has occurred!");
+        } else {
+            dupes   = (rowerrors.indexOf(1) === -1) ? (colerrors.indexOf(1) === -1) ? (regerrors.indexOf(1) === -1) ? "" : "" : "" : "<li>Numbers cannot be repeated in each row, column or region</li>";
+            empties = (rowerrors.indexOf(2) === -1) ? (colerrors.indexOf(2) === -1) ? (regerrors.indexOf(2) === -1) ? "" : "" : "" : "<li>Uh oh, there are empty cells!</li>";
+            if (dupes !== "" || empties !== "") {
+                $("#status").addClass("error").html("<ul>" + dupes + empties + "</ul>");
+            } else {
+                $("#status").addClass("solved").html("Solved!");
+            }
+        }
+        // debug:
+        $("#status").append("<br /><br />rowerrors: " + rowerrors + "<br />colerrors: " + colerrors + "<br />regerrors: " + regerrors);
+    },
+    analyze: function(type, i) {
+        var nums = [],
+            sum  = 0,
+            num;
+        $.each($("." + type + i + " input"), function(){
+            num = parseInt($(this).val());
+            // make sure it's a number, it's not infinity, it's not a decimal, it's between 0 and 10
+            if (typeof num === "number" && isFinite(num) && num % 1 === 0 && num > 0 && num < 10) {
+                // make sure number is unique
+                if ($.inArray(num, nums) === -1) {
+                    nums.push(num);
+                    sum += num;
+                } else {
+                    nums.push(0);
+                }
+            }
+        });
+        // validate length
+        if (nums.length === 9) {
+            // should total 45 (if not, a number was duplicated somewhere in this collection)
+            if (sum === 45) {
+                // all good
+                return 0;
+            } else {
+                // number duplicated in col
+                return 1;
+            }
+        } else {
+            // not enough valid numbers in col
+            return 2;
+        }
+    }
+};
 
-        $("#status").html(
-            "rowerrors: " + rowerrors + 
-            "<br />colerrors: " + colerrors + 
-            "<br />sqrerrors: " + sqrerrors);
+$(function(){
+
+    // validate board
+    $("#btnvalidate").on("click", function() {
+        sudoku.validate();
+    });
+
+
+    // prevent unwanted characters, and detect remaining empties at the same time
+    // bind to "input", as "change" is deferred until the input loses focus
+    $("input").on("input", function() {
+        // limit to nums 1-9
+        $(this).val($(this).val().replace(/[^1-9]/,""));
+        // how many empties are left?
+        var empties = 0;
+        $("#board input").each(function() {
+            if($(this).val() === "") {
+                empties++;
+            }
+        });
+        if (empties === 0) {
+            sudoku.validate();
+        } else {
+            $("#status").append(empties + "|");
+        }
+    });
+
+
+    // make a cell active on click (also select the text for easy modifcation)
+    $("#board tr td").on("click", function() {
+        $("#board tr td").removeClass("active");
+        $(this).addClass("active");
+        if ($(this).hasClass("empty")) {
+            $(this).find("input").select();
+        }
+    });
+    // ...or on input focus
+    $("#board tr td input").on("focus", function() {
+        $("#board tr td").removeClass("active");
+        $(this).parent().addClass("active");
+    });
+
+
+    // insert a number in the active cell on click
+    $("#numbers span").on("click", function() {
+        $("#board tr td.active input").val($(this).attr("data-num"));
     });
 
 
     // load a new board
-    $("#btnnew").on("click", function(){
+    $("#btnnew").on("click", function() {
         sudoku.generate();
-    })
+    });
 
 
-
-
+    // generate a playable board once on page load
+    sudoku.generate();
 
 
 });
