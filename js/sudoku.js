aacaa71dc734e764986166024c4eb023d9f9bef3
stereobird <stereobird@gmail.com>
2015-03-24 11:11:08 -0700

    Validation
    I'm rethinking the board generation, will look into backtracking schemes
    later

--- js/sudoku.js
+++ js/sudoku.js
@@ -1,21 +1,32 @@
-var sudoku = {
+
+(function(sudoku, $, undefined) {
+
+    sudoku.init = function() {
+        sudoku.generate();
+    };
+
+
     // generate a basic number grid (81 numbers in a "solved" but generic state)
-    generate: function() {
+    sudoku.generate = function() {
         var nums = [],
             i,
             ii;
         for (i = 0; i < 9; i++) {
             for (ii = 0; ii < 9; ii++) {
-                nums.push((i*3 + Math.floor(i/3) + ii) % 9 + 1);
+                nums.push((i * 3 + Math.floor(i / 3) + ii) % 9 + 1);
             }
         }
         sudoku.refresh(sudoku.disorder(nums));
         $("#status").html("");
-    },
+        $("td").removeClass("corrected entered valid");
+    };
+
+
+
     // mixup the numbers in the array, but sort of thoughtfully
-    disorder: function(nums) {
+    sudoku.disorder = function(nums) {
         var i,
-            freq = 75,
+            freq = 66,
             orig,
             dest,
             ii;
@@ -31,98 +42,91 @@ var sudoku = {
             }
         }
         return nums;
-    },
-    refresh: function(nums) {
+    };
+
+
+
+    sudoku.refresh = function(nums) {
         var rnd;
+        key = nums;
         // very crudely, we'll just randomize hiding of some numbers
         $.each(nums, function(i,v) {
             rnd = Math.random();
-            // ~30% of the time, don't insert the number
-            if (rnd > 0.32) {
-                $("#x" + (i+1)).val(v);
-                $("#x" + (i+1)).attr("type", "text").attr("readonly", "readonly").parent().removeClass("empty").addClass("filled");
+            // ~33% of the time, don't insert the number
+            if (rnd > 0.33) {
+                $("#x" + (i + 1)).val(v);
+                $("#x" + (i + 1)).attr("type", "text").attr("readonly", "readonly").parent().removeClass("empty").addClass("filled");
             } else {
-                $("#x" + (i+1)).val("");
-                $("#x" + (i+1)).removeAttr("readonly").parent().removeClass("filled").addClass("empty");
+                $("#x" + (i + 1)).val("");
+                $("#x" + (i + 1)).removeAttr("readonly").parent().removeClass("filled").addClass("empty");
             }
 
         });
-    },
-    validate: function() {
-        var rowerrors = [],
-            colerrors = [],
-            regerrors = [],
-            i,
-            dupes     = "",
-            empties   = "";
-        // loop throuh all rows/cols/regions 9x
-        for (i = 1; i < 10; ++i) {
-            // check row
-            rowerrors.push(sudoku.analyze("row", i));
-            // check column
-            colerrors.push(sudoku.analyze("col", i));
-            // check region
-            regerrors.push(sudoku.analyze("reg", i));
-        }
-        // all zeroes means no errors in the row, column or region; e.g. [0,0,0,0,0,0,0,0,0]
-        // 1 indicates a duplicated number in the row, column or region; e.g. [0,0,0,1,0,1,1,1,0]
-        // 2 indictaes an empty space in the row, column or region; e.g. [0,0,2,2,0,0,0,0,0]
-        // (2 takes precedence)
-        if (rowerrors.length !== 9 || colerrors.length !== 9 || regerrors.length !== 9) {
-            // something has gone wrong, one or more groups could not be validated
-            // (this should never happen)
-            $("#status").html("A horrible error has occurred!");
-        } else {
-            dupes   = (rowerrors.indexOf(1) === -1) ? (colerrors.indexOf(1) === -1) ? (regerrors.indexOf(1) === -1) ? "" : "" : "" : "<li>Numbers cannot be repeated in each row, column or region</li>";
-            empties = (rowerrors.indexOf(2) === -1) ? (colerrors.indexOf(2) === -1) ? (regerrors.indexOf(2) === -1) ? "" : "" : "" : "<li>Uh oh, there are empty cells!</li>";
-            if (dupes !== "" || empties !== "") {
-                $("#status").addClass("error").html("<ul>" + dupes + empties + "</ul>");
+    };
+
+
+
+    sudoku.validate = function(solve) {
+        board.length = 0;
+        var i,
+            ii = key.length,
+            cell,
+            errors = 0,
+            msg;
+        for (i = 0; i < 81; i++) {
+            if ($("#x" + (i + 1)).val() != "") {
+                board.push($("#x" + (i + 1)).val());
             } else {
-                $("#status").addClass("solved").html("Solved!");
+                board.push(0);
             }
         }
-        // debug:
-        $("#status").append("<br /><br />rowerrors: " + rowerrors + "<br />colerrors: " + colerrors + "<br />regerrors: " + regerrors);
-    },
-    analyze: function(type, i) {
-        var nums = [],
-            sum  = 0,
-            num;
-        $.each($("." + type + i + " input"), function(){
-            num = parseInt($(this).val());
-            // make sure it's a number, it's not infinity, it's not a decimal, it's between 0 and 10
-            if (typeof num === "number" && isFinite(num) && num % 1 === 0 && num > 0 && num < 10) {
-                // make sure number is unique
-                if ($.inArray(num, nums) === -1) {
-                    nums.push(num);
-                    sum += num;
+        if (solve === "solve") {
+            // solve it!
+            while(ii--) {
+                cell = $("#x" + (ii + 1));
+                if (board[ii] == 0) {
+                    cell.val(key[ii]);
+                    if (cell.parent().hasClass("empty")) {
+                        cell.parent().removeClass("active").addClass("entered");
+                    }
+                } else if (key[ii] != board[ii]) {
+                    cell.val(key[ii]);
+                    if (cell.parent().hasClass("empty")) {
+                        cell.parent().removeClass("active").addClass("corrected");
+                    }
                 } else {
-                    nums.push(0);
+                    if (cell.parent().hasClass("empty")) {
+                        cell.parent().removeClass("active").addClass("valid");
+                    }
                 }
             }
-        });
-        // validate length
-        if (nums.length === 9) {
-            // should total 45 (if not, a number was duplicated somewhere in this collection)
-            if (sum === 45) {
-                // all good
-                return 0;
-            } else {
-                // number duplicated in col
-                return 1;
-            }
         } else {
-            // not enough valid numbers in col
-            return 2;
+            // just check validity
+            while(ii--) {
+                if (board[ii] == 0) {
+                    errors += 1;
+                } else if (key[ii] != board[ii]) {
+                    errors += 1;
+                }
+            }
+            msg = (errors > 0) ? (errors > 5) ? "Uh oh. There's a few mistakes." : "Almost perfect!" : "Solved!"
+            $("#status").html(msg);
         }
-    }
-};
+    };
+
+
+}(window.sudoku = window.sudoku || {}, jQuery));
+
+
+
+var key   = [],
+    board = [];
 
 $(function(){
 
     // validate board
     $("#btnvalidate").on("click", function() {
-        sudoku.validate();
+        sudoku.validate("solve");
     });
 
 
@@ -139,7 +143,7 @@ $(function(){
             }
         });
         if (empties === 0) {
-            sudoku.validate();
+            sudoku.validate("check");
         } else {
             $("#status").append(empties + "|");
         }
@@ -174,7 +178,8 @@ $(function(){
 
 
     // generate a playable board once on page load
-    sudoku.generate();
+    //sudoku.generate();
+    sudoku.init();
 
 
 });
